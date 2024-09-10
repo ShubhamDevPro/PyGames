473ef528ab49a497488675177d6f6f3d6d334296
Shubham Dev <devshubham22@gmail.com>
2024-09-10 12:51:48 +0530

    .
    .

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+x= pygame.init()
+gameWindow = pygame.display.set_mode((1200, 500))
+pygame.display.set_caption("My First Game")
+pygame.display.update()
+
+gameExit = False
+
+while not gameExit:
+    for event in pygame.event.get():
+        print(event)
+
+pygame.quit()
+print(x)
