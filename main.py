3fda817ab6218827aec69a82b67adcd31a0f287f
Shubham Dev <shubhamdev@Shubhams-MacBook-Air-2.local>
2024-09-10 12:55:25 +0530

    ok

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ x= pygame.init()
 gameWindow = pygame.display.set_mode((1200, 500))
 pygame.display.set_caption("My First Game")
 pygame.display.update()
-
+# Game specific variables
 gameExit = False
-
+gameOver = False
 while not gameExit:
     for event in pygame.event.get():
         print(event)
